006d60fea3bf5a972fff17793135b70dfa2cb68d
Mohammad Ruhul Amin <mruhul@gmail.com>
2023-06-07 20:30:12 +1000

    Add iocsetup

---
+++ src/Bolt.DynamoDbClient/IocSetup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Bolt.DynamoDbClient;
+
+public static class IocSetup
+{
+    public static IServiceCollection AddBoltDynamoDbClient(this IServiceCollection services)
+    {
+        services.TryAddTransient<IDynamoDbWrapper, DynamoDbWrapper>();
+
+        return services;
+    }
+}
